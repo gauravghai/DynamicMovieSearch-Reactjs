3aa0db5a770cf8ca9ec8f284d3c1b7338d789087
gauravvghai <49611619+gauravvghai@users.noreply.github.com>
2021-10-22 16:09:30 +0530

    Final Update

--- src/components/Movies/CardImage.js
+++ src/components/Movies/CardImage.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import NoThumbanil from '../../assets/NoThubnail.png'
 
 function CardImage(props) {
+
+    const [image, setImage] = useState(props.value)
     // If image status is 404 then showing placeholder image 
   return (
-    <img className="Card-Image" src={props.value} onError={(e)=>{e.target.onError = null; e.target.src = NoThumbanil}}/> 
+    <img className="Card-Image" src={image} alt={props.title} onError={(e)=>{e.target.onError = null; setImage(NoThumbanil)}}/> 
   );
 }
 
