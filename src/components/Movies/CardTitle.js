e07ae0ae945a4c806240641dda8cfb20815da812
gauravvghai <49611619+gauravvghai@users.noreply.github.com>
2021-10-22 14:12:44 +0530

    SEO Changes & Readme file checkin

--- src/components/Movies/CardTitle.js
+++ src/components/Movies/CardTitle.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function CardTitle(props) {
   return (
-    <h1 className="card-title">
+    <h2 className="card-title">
       {props.value}
-    </h1>
+    </h2>
   );
 }
 
